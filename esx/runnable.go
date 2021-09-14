a6e9cd1c72f590c76817869112204a5dab7c48ca
D074096 <erik.schubert@sap.com>
2021-09-14 16:11:06 +0200

    Add missing pod permissions

--- esx/runnable.go
+++ esx/runnable.go
@@ -32,6 +32,7 @@ import (
 )
 
 // +kubebuilder:rbac:groups=core,resources=nodes,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=core,resources=nodes/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=core,resources=events,verbs=create;update;patch
 
