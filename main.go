edecfb32b69eb1825e97bd20fb2da52c73c17d7a
D074096 <erik.schubert@sap.com>
2022-04-05 16:08:37 +0200

    Wait for metrics scrape before terminating

--- main.go
+++ main.go
@@ -61,19 +61,29 @@ func init() {
 	//+kubebuilder:scaffold:scheme
 }
 
+type reconcilerConfig struct {
+	metricsAddr                 string
+	metricsTimeout              time.Duration
+	enableESXMaintenance        bool
+	enableKubernikusMaintenance bool
+}
+
 func main() {
-	var metricsAddr, kubecontext, probeAddr string
-	var enableLeaderElection, enableESXMaintenance, enableKubernikusMaintenance bool
-	flag.StringVar(&metricsAddr, "metrics-addr", ":8080", "The address the metric endpoint binds to.")
+	var reconcilerConfig reconcilerConfig
+	var kubecontext, probeAddr string
+	var enableLeaderElection bool
+	flag.StringVar(&reconcilerConfig.metricsAddr, "metrics-addr", ":8080", "The address the metric endpoint binds to.")
 	flag.StringVar(&kubecontext, "kubecontext", "", "The context to use from the kubeconfig (defaults to current-context)")
 	flag.StringVar(&probeAddr, "health-addr", ":8081", "The address the probe endpoint binds to.")
 	flag.BoolVar(&enableLeaderElection, "enable-leader-election", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
-	flag.BoolVar(&enableESXMaintenance, "enable-esx-maintenance", false,
+	flag.BoolVar(&reconcilerConfig.enableESXMaintenance, "enable-esx-maintenance", false,
 		"Enables an additional controller, which will indicate ESX host maintenance using labels.")
-	flag.BoolVar(&enableKubernikusMaintenance, "enable-kubernikus-maintenance", false,
+	flag.BoolVar(&reconcilerConfig.enableKubernikusMaintenance, "enable-kubernikus-maintenance", false,
 		"Enables an additional controller, which will indicate outdated kubelets and enable VM deletions.")
+	flag.DurationVar(&reconcilerConfig.metricsTimeout, "metrics-timeout", 65*time.Second, //nolint:gomnd
+		"Maximum delay between SIGTERM and actual shutdown to scrape metrics one last time.")
 	opts := zap.Options{
 		Development: true,
 		TimeEncoder: zapcore.RFC3339TimeEncoder,
@@ -86,9 +96,10 @@ func main() {
 	setupLog.Info("Loaded kubeconfig", "context", kubecontext, "host", restConfig.Host)
 
 	leaderElectionRetry := 5 * time.Second //nolint:gomnd
+	shutdownTimeout := 70 * time.Second    //nolint:gomnd
 	mgr, err := ctrl.NewManager(restConfig, ctrl.Options{
 		Scheme:                     scheme,
-		MetricsBindAddress:         metricsAddr,
+		MetricsBindAddress:         "0",  // disable inbuilt metrics server
 		Port:                       9443, //nolint:gomnd
 		HealthProbeBindAddress:     probeAddr,
 		EventBroadcaster:           event.NewNodeBroadcaster(),
@@ -96,6 +107,7 @@ func main() {
 		LeaderElection:             enableLeaderElection,
 		LeaderElectionID:           "maintenance-controller-leader-election.cloud.sap",
 		RetryPeriod:                &leaderElectionRetry,
+		GracefulShutdownTimeout:    &shutdownTimeout,
 	})
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
@@ -104,7 +116,7 @@ func main() {
 
 	metrics.RegisterMaintenanceMetrics()
 	setupChecks(mgr)
-	err = setupReconcilers(mgr, enableESXMaintenance, enableKubernikusMaintenance)
+	err = setupReconcilers(mgr, &reconcilerConfig)
 	if err != nil {
 		setupLog.Error(err, "problem setting up reconcilers")
 		os.Exit(1)
@@ -141,7 +153,7 @@ func setupChecks(mgr manager.Manager) {
 	}
 }
 
-func setupReconcilers(mgr manager.Manager, enableESXMaintenance bool, enableKubernikusMaintenance bool) error {
+func setupReconcilers(mgr manager.Manager, cfg *reconcilerConfig) error {
 	if err := (&controllers.NodeReconciler{
 		Client:   mgr.GetClient(),
 		Log:      ctrl.Log.WithName("controllers").WithName("maintenance"),
@@ -163,7 +175,16 @@ func setupReconcilers(mgr manager.Manager, enableESXMaintenance bool, enableKube
 		return fmt.Errorf("Unable to create index spec.nodeName on pod resource: %w", err)
 	}
 
-	if enableKubernikusMaintenance {
+	prom := metrics.PromServer{
+		Address:     cfg.metricsAddr,
+		Log:         ctrl.Log.WithName("metrics"),
+		WaitTimeout: cfg.metricsTimeout,
+	}
+	if err := mgr.Add(&prom); err != nil {
+		return fmt.Errorf("Failed to attach prometheus metrics server: %w", err)
+	}
+
+	if cfg.enableKubernikusMaintenance {
 		setupLog.Info("Kubernikus integration is enabled")
 		if err := (&kubernikus.NodeReconciler{
 			Client: mgr.GetClient(),
@@ -175,7 +196,7 @@ func setupReconcilers(mgr manager.Manager, enableESXMaintenance bool, enableKube
 		}
 	}
 
-	if enableESXMaintenance {
+	if cfg.enableESXMaintenance {
 		setupLog.Info("ESX integration is enabled")
 		controller := esx.Runnable{
 			Client: mgr.GetClient(),
---
+++ metrics/server.go
@@ -0,0 +1,82 @@
+/*******************************************************************************
+*
+* Copyright 2020 SAP SE
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You should have received a copy of the License along with this
+* program. If not, you may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*******************************************************************************/
+
+// We run our on prometheus serving logic here, to "ensure" a last scrape in case
+// the maintenance-controller drains itself. Otherwise shuffle metrics are inaccurate.
+
+package metrics
+
+import (
+	"context"
+	"net"
+	"net/http"
+	"time"
+
+	"github.com/go-logr/logr"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"sigs.k8s.io/controller-runtime/pkg/metrics"
+)
+
+type PromServer struct {
+	Address     string
+	WaitTimeout time.Duration
+	Log         logr.Logger
+	counter     int
+}
+
+func (ps *PromServer) NeedLeaderElection() bool {
+	return false
+}
+
+func (ps *PromServer) Start(ctx context.Context) error {
+	listener, err := net.Listen("tcp", ps.Address)
+	if err != nil {
+		return err
+	}
+	handler := promhttp.HandlerFor(metrics.Registry, promhttp.HandlerOpts{
+		ErrorHandling: promhttp.HTTPErrorOnError,
+	})
+	mux := http.NewServeMux()
+	mux.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+		ps.counter++
+		handler.ServeHTTP(w, r)
+	})
+	// values copied over from controller-runtime
+	server := &http.Server{
+		Handler:        mux,
+		MaxHeaderBytes: 1 << 20, //nolint:gomnd
+		// matches http.DefaultTransport keep-alive timeout
+		IdleTimeout:       90 * time.Second, //nolint:gomnd
+		ReadHeaderTimeout: 32 * time.Second, //nolint:gomnd
+	}
+	go func() {
+		_ = server.Serve(listener)
+	}()
+	<-ctx.Done()
+	last := ps.counter
+	ps.Log.Info("Awaiting an other metrics scrape", "timeout", ps.WaitTimeout)
+	_ = wait.PollImmediate(1*time.Second, ps.WaitTimeout, func() (done bool, err error) {
+		return ps.counter > last, nil
+	})
+	timeout, cancel := context.WithTimeout(context.Background(), 5*time.Second) //nolint:gomnd
+	err = server.Shutdown(timeout)
+	cancel()
+	return err
+}
